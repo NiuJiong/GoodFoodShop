c6c6ec6d8b4d5b61729b351e08001b792c8ecd43
lgdcnm <18268815524@163.com>
2017-08-18 16:13:34 +0800

    one

---
+++ js/index.js
@@ -0,0 +1,25 @@
+$(function(){
+	
+//导航效果
+		
+	if($(window).width()<992){
+		$("header nav ul").hide();
+	}
+	$("header nav .menu").click(function(){
+		$("header nav ul").slideToggle();
+	});
+
+//	轮播
+	var index = 0;
+	setInterval(function(){
+		$(".banner div").removeClass("active");
+		$(".banner div:eq("+index+")").addClass("active");
+		index++;
+		if(index >= $(".banner div").length){
+			index=0;
+		}
+	},4000);
+	
+	
+	
+});
